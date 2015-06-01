92833c7e286f2079cf19d6a82a686413520c8d3f
tziporaziegler <tziporaziegler@gmail.com>
2015-06-01 02:22:24 -0400

    finish commit

---
+++ src/dropbox/ListMessage.java
@@ -0,0 +1,46 @@
+package dropbox;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ListMessage extends Message {
+
+	// LIST
+	private final static Pattern PATTERN = Pattern.compile("LIST");
+
+	@Override
+	public boolean matches(String msg) {
+		Matcher matcher = PATTERN.matcher(msg);
+		return matcher.matches();
+	}
+
+	@Override
+	public void perform(FileCache cache, Socket socket) {
+		try {
+			File folder = new File(cache.getRoot());
+
+			// get list of all files in root directory
+			File[] listOfFiles = folder.listFiles();
+
+			// check if directory is empty
+			if (listOfFiles != null) {
+				// send message with the number of files
+				send("FILES " + listOfFiles.length, socket);
+
+				// for each file message with the data
+				for (File file : listOfFiles) {
+					if (file.isFile()) {
+						send("FILE " + file.getName() + " " + file.lastModified() + " " + file.length() + " ", socket);
+					}
+				}
+			}
+
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
