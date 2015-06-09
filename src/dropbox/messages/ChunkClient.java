c20f12b79c89a82ea167888282e49ba4ad4b13e4
tziporaziegler <tziporaziegler@gmail.com>
2015-06-09 00:50:47 -0400

    Revert "chunk client change"
    This reverts commit 242d994c6f8f6c1edf356224137b2194930d6bbf.

--- src/dropbox/messages/ChunkClient.java
+++ src/dropbox/messages/ChunkClient.java
@@ -1,20 +1,16 @@
 package dropbox.messages;
 
-import java.io.IOException;
 import java.net.Socket;
-import java.util.ArrayList;
 
 import dropbox.FileCache;
 import dropbox.Server;
 
 public class ChunkClient extends ChunkMessage {
 	private Server server;
-	private ArrayList<Socket> sockets;
 
 	public ChunkClient(Server server) {
 		super(server);
 		this.server = server;
-		
 	}
 
 	@Override
@@ -23,19 +19,5 @@ public class ChunkClient extends ChunkMessage {
 		String[] splitMsg = msg.split(" ");
 		server.setSyncMsg("SYNC " + splitMsg[1] + " " + splitMsg[2] + " " + splitMsg[3]);
 		createFile(splitMsg);
-		
-//		sockets = server.getSockets();
-//		// send SYNC message to all sockets in arrat
-//		for (Socket currentSock : sockets) {
-//			//if (currentSock != socket) {
-//				try {
-//					send(("SYNC " + splitMsg[1] + " " + splitMsg[2] + " " + splitMsg[3]), currentSock);
-//				}
-//				catch (IOException e) {
-//					e.printStackTrace();
-//				}
-//			//}
-	//	}
-	
 	}
 }
